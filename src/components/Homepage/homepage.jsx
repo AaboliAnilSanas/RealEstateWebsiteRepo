7a8977cb73f81a32affa0860b2be262c27715335
aabolisanas23 <aabolisanas23@gmail.com>
2025-11-02 19:01:57 +0530

    sell property UI

--- src/components/Homepage/homepage.jsx
+++ src/components/Homepage/homepage.jsx
@@ -8,14 +8,6 @@ import PropertyListingBanner from "./SellPropertyBanner/PropertyListingBanner";
 const HomePage = () => {
   return (
     <div className="min-h-screen flex flex-col overflow-hidden relative">
-      {/* Background SVG Container */}
-
-      {/* Content Container */}
-      {/* <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        <div className="absolute top-45 -left-40 w-96 h-96 rounded-full bg-gradient-to-r from-yellow-200 to-orange-200 opacity-25 blur-3xl animate-pulse"></div>
-        <div className="absolute top-45 -right-40 w-96 h-96 rounded-full bg-gradient-to-r from-purple-700 to-pink-200 opacity-25 blur-3xl animate-pulse" style={{animationDelay: '2s'}}></div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 rounded-full bg-gradient-to-r from-blue-100 to-cyan-100 opacity-20 blur-3xl"></div>
-      </div> */}
       <div className="relative z-10">
         {/* Video Section */}
         <div className="relative z-20   bg-gradient-to-br from-gray-50  from-gray-50 ">
@@ -24,12 +16,6 @@ const HomePage = () => {
           <FilterSearchPage />
         </div>
         </div>
-        
-        {/* Filter Section
-        <div className="mx-[150px] mb-20 -mt-55 z-30 relative rounded-2xl ring-5 ring-inset ring-gray-200 bg-white">
-          <FilterSearchPage />
-        </div> */}
-
         <LocationSection />
         <PropertyListingBanner/>
         <Footer />
